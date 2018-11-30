6ef8926db27b9757cc8cfbaa408b7921bf18fc17
Venki Korukanti <venkik@uber.com>
2018-11-30 12:30:14 -0800

    Print long running split details
    Summary:
    Currently we get a warning message in logs a split has been running for long, but
    we don't know which datanodes the split is talking to. We can get the datanodes if the
    query and the problemantic split is still running. This diff adds the split info warning
    message, so that we get a pattern of problematic datanodes just by looking at the logs
    
    Test Plan:
    Test locally with debug breakpoint. Here is the sample split info:
    `Split 20181130_215957_00012_fuz6x.1.0-0 {path=hdfs://venkik-C02X74DUJGH6:8020/hive232/kv_orc_hdfs/000000_0, start=0, length=65057, fileSize=65057, hosts=[localhost], database=default, table=kv_orc_hdfs, forceLocalScheduling=false, partitionName=<UNPARTITIONED>, nestedFields=Optional.empty, nestedTupleDomain=Optional.empty, aggregations=Optional.empty} (start = 0.0, wall = 426857431 ms, cpu = 0 ms, wait = 0 ms, calls = 0)`
    
    Reviewers: dharakk
    
    Reviewed By: dharakk
    
    Subscribers: jenkins
    
    Maniphest Tasks: T2288069
    
    Differential Revision: https://code.uberinternal.com/D4135925

--- presto-main/src/main/java/com/facebook/presto/execution/executor/TaskExecutor.java
+++ presto-main/src/main/java/com/facebook/presto/execution/executor/TaskExecutor.java
@@ -478,7 +478,7 @@ public class TaskExecutor
 
                     String threadId = split.getTaskHandle().getTaskId() + "-" + split.getSplitId();
                     try (SetThreadName splitName = new SetThreadName(threadId)) {
-                        RunningSplitInfo splitInfo = new RunningSplitInfo(ticker.read(), threadId, Thread.currentThread());
+                        RunningSplitInfo splitInfo = new RunningSplitInfo(ticker.read(), threadId, Thread.currentThread(), split.getInfo());
                         runningSplitInfos.add(splitInfo);
                         split.getTaskHandle().getSessionLogger().log(() -> "running split " + split);
                         runningSplits.add(split);
@@ -816,6 +816,12 @@ public class TaskExecutor
                 for (StackTraceElement traceElement : splitInfo.getThread().getStackTrace()) {
                     stackTrace.append("\tat ").append(traceElement).append("\n");
                 }
+                if (!splitInfo.isPrinted()) {
+                    splitInfo.setPrinted();
+                    Exception exception = new Exception("Long running split");
+                    exception.setStackTrace(splitInfo.getThread().getStackTrace());
+                    log.warn(exception, "Split thread %s has been running longer than %s. Split details: %s", splitInfo.getThreadId(), duration, splitInfo.getDetails());
+                }
             }
         }
 
@@ -841,13 +847,15 @@ public class TaskExecutor
         private final long startTime;
         private final String threadId;
         private final Thread thread;
+        private final String details;
         private boolean printed;
 
-        public RunningSplitInfo(long startTime, String threadId, Thread thread)
+        public RunningSplitInfo(long startTime, String threadId, Thread thread, String details)
         {
             this.startTime = startTime;
             this.threadId = threadId;
             this.thread = thread;
+            this.details = details;
             this.printed = false;
         }
 
@@ -864,6 +872,11 @@ public class TaskExecutor
         public Thread getThread()
         {
             return thread;
+        }
+
+        public String getDetails()
+        {
+            return details;
         }
 
         public boolean isPrinted()
